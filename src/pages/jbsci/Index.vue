3c58f626ef2ec3df3a83b9632254a6d03f08a3ba
litthy <litthy@qq.com>
2024-01-05 14:29:57 +0800

    feat: slider

--- src/pages/jbsci/Index.vue
+++ src/pages/jbsci/Index.vue
@@ -2,11 +2,14 @@
     <div class="p-event-content">
         <Mark v-if="mark" @close="closeMark" />
         <Tabs @update="showComponent" />
-        <component :is="active" />
+        <component :is="active" :data="componentData" />
     </div>
 </template>
 
 <script>
+const KEY = "jbsci";
+import { getTopic } from "@/service/topic";
+
 import Mark from "./components/mark.vue";
 import Tabs from "./components/tabs.vue";
 import INDEX from "./components/index.vue";
@@ -19,11 +22,47 @@ export default {
     components: { Mark, Tabs },
     data: function () {
         return {
+            raw: [],
             mark: false,
             active: INDEX,
+            key: "INDEX",
+
+            slider: [],
         };
     },
+    computed: {
+        data() {
+            let _data = {};
+            this.raw.forEach((item) => {
+                if (!_data[item.subtype]) {
+                    _data[item.subtype] = [];
+                }
+                _data[item.subtype].push(item);
+            });
+            Object.keys(_data).forEach((key) => {
+                _data[key] = _data[key].sort((a, b) => a.power - b.power);
+            });
+            return _data;
+        },
+        componentData() {
+            const _data = {
+                ARTICLES: {},
+                AUTHORS: {},
+                INDEX: {
+                    slider: this.slider,
+                },
+            };
+            return _data[this.key];
+        },
+    },
     methods: {
+        init() {
+            getTopic(KEY).then((res) => {
+                this.raw = res.data.data;
+                const { slider } = this.data;
+                this.slider = slider;
+            });
+        },
         closeMark() {
             setTimeout(() => {
                 this.mark = false;
@@ -35,8 +74,12 @@ export default {
                 AUTHORS,
                 INDEX,
             };
+            this.key = name;
             this.active = data[name];
         },
+    },
+    mounted() {
+        this.init();
     },
 };
 </script>
--- src/pages/jbsci/components/index.vue
+++ src/pages/jbsci/components/index.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="index">
+        <el-carousel height="680px">
+            <el-carousel-item v-for="(item, i) in slider" :key="i">
+                <a :href="item.link" target="_blank">
+                    <el-image :src="item.img" fit="fill"></el-image>
+                </a>
+            </el-carousel-item>
+        </el-carousel>
         <div class="footer"></div>
     </div>
 </template>
@@ -7,10 +14,44 @@
 <script>
 export default {
     name: "index",
+    props: ["data"],
     data: function () {
-        return {};
+        return {
+            slider: [],
+        };
     },
-
-    computed: {},
+    watch: {
+        data: {
+            deep: true,
+            immediate: true,
+            handler: function ({ slider }) {
+                if (slider) {
+                    this.slider = slider;
+                }
+            },
+        },
+    },
 };
 </script>
+<style lang="less">
+.index {
+    .el-carousel__indicators--horizontal {
+        left: 20px;
+    }
+    .el-carousel__indicator--horizontal .el-carousel__button {
+        .size(10px);
+        .r(50%);
+        background: #fff;
+        border: 1px solid #fff;
+        border-radius: 50%;
+        opacity: 0.3;
+    }
+    .el-carousel__indicator--horizontal.is-active .el-carousel__button {
+        .size(10px);
+        .r(50%);
+        background: #ba9624;
+        border: 1px solid #ba9624;
+        opacity: 1;
+    }
+}
+</style>
