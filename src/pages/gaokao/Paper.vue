cfb28daebeaf9be1ce322be08c8f57491ce210cf
litthy <litthy@qq.com>
2024-05-24 14:26:42 +0800

    fix: year

--- src/pages/gaokao/Paper.vue
+++ src/pages/gaokao/Paper.vue
@@ -3,7 +3,7 @@
         <div class="m-header">
             <img :src="__imgRoot + `logo-${logoNumber}.svg`" class="u-text u-logo" alt="魔盒" />
             <img :src="__imgRoot + `font-${textNumber}.svg`" class="u-text u-text-1" alt="全力以赴" />
-            <img :src="__imgRoot + `${year}.png`" class="u-text u-year" v-if="year !== '2022'" />
+            <img :src="__imgRoot + `${year}.png`" class="u-text u-year" />
             <img :src="__imgRoot + 'text.png'" class="u-text u-text-2" :style="{ filter }" alt="2023剑三高考卷" />
         </div>
         <div class="m-exam">
