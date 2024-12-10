d7021a208382ae6af3687c861791c35da5a259e1
Jerry <TiAmo_me@proton.me>
2024-12-11 01:16:06 +0800

    feat:页面

--- src/pages/birthday/birthday5.vue
+++ src/pages/birthday/birthday5.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="m-content">
-        <div class="u-top__bg"></div>
         <div class="m-page-tabs">
             <div class="u-slider"></div>
-            <div class="u-item" v-for="(item, index) in items" :key="index">{{ item }}</div>
+            <div class="m-item" v-for="(item, index) in 8" :key="index">
+                <img class="u-img" :src="require(`../../assets/img/celebration/nav/${item}.svg`)" alt="">
+            </div>
         </div>
         <div class="m-page-0">
+            <div class="u-top__bg"></div>
             <div class="m-img-box">
                 <div v-for="(item, index) in 12" :key="index">
                     <img
                         class="u-img"
                         :src="require(`../../assets/img/celebration/0/${index}.png`)"
                         alt=""
-                        @mouseover="playAudio"
+                        @mouseover="playAudio(item)"
                     />
                 </div>
             </div>
@@ -44,7 +46,7 @@
                 <img class="u-title" src="../../assets/img/celebration/2/title.svg" alt="">
             </div>
             <div class="u-face" :class="`u-face-${item}`" v-for="(item,index) in 3" :key="item">
-                <img class="u-img" :src="require(`../../assets/img/celebration/1/Ellipse${index}.png`)" alt="">
+                <img class="u-img" :src="require(`../../assets/img/celebration/2/Ellipse${index}.png`)" alt="">
                 <img class="u-code" src="../../assets/img/celebration/miniprogram.jpg" alt="">
             </div>
         </div>
@@ -164,12 +166,176 @@
                             <div class="u-plate__title">魔盒5周年限定主题·评论</div>
                         </div>
                     </div>
+
+                    <div class="m-item u-card">
+                        <div class="m-info m-card">
+                            <div class="m-plate__content"></div>
+                            <div class="m-info-hover">
+                                <div class="u-plate__title">魔盒5周年限定主题·评论</div>
+                                <div class="u-plate__sub">实装位置：魔盒官网-首页-最新动态上方</div>
+                                <div class="m-plate__tip">
+                                    <div class="u-tip__box">
+                                        <div class="u-tip__title">仅限魔盒会员领取</div>
+                                        <div class="u-tip__time">领取时间：2024.12.28~2025.2.28</div>
+                                    </div>
+                                    <div>
+                                        <img class="u-tip__get" src="../../assets/img/celebration/get.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-plate__title">
+                            <div class="u-plate__title">魔盒5周年限定主题·评论</div>
+                        </div>
+                    </div>
+
+                    <div class="m-item u-avatar">
+                        <div class="m-info m-avatar">
+                            <div class="m-plate__content"></div>
+                            <div class="m-info-hover">
+                                <div class="u-plate__title">魔盒5周年限定主题·评论</div>
+                                <div class="u-plate__sub">实装位置：魔盒官网-首页-最新动态上方</div>
+                                <div class="m-plate__tip">
+                                    <div class="u-tip__box">
+                                        <div class="u-tip__title">仅限魔盒会员领取</div>
+                                        <div class="u-tip__time">领取时间：2024.12.28~2025.2.28</div>
+                                    </div>
+                                    <div>
+                                        <img class="u-tip__get" src="../../assets/img/celebration/get.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-plate__title">
+                            <div class="u-plate__title">魔盒5周年限定主题·评论</div>
+                        </div>
+                    </div>
+
+                    <img class="u-getAll" src="../../assets/img/celebration/gets.png" alt="">
                 </div>
             </div>
             <div class="u-face" :class="`u-face-${item}`" v-for="(item,index) in 4" :key="item">
-                <img class="u-img" :src="require(`../../assets/img/celebration/1/Ellipse${index}.png`)" alt="">
+                <img class="u-img" :src="require(`../../assets/img/celebration/3/Ellipse${index}.png`)" alt="">
                 <img class="u-code" src="../../assets/img/celebration/miniprogram.jpg" alt="">
             </div>
+        </div>
+
+        <div class="m-page-4 m-page-layout">
+            <img class="u-mark" src="../../assets/img/celebration/4/4.svg" alt="">
+            <img class="u-bg" src="../../assets/img/celebration/4/bg.jpg" alt="">
+            <div class="m-main">
+                <img class="u-title" src="../../assets/img/celebration/4/title.svg" alt="">
+                <div class="m-fun">
+                    <div class="m-item" v-for="(item,index) in page4Arr" :key="index">
+                        <img class="u-fun__img" :src="require(`../../assets/img/celebration/4/V${index+1}.svg`)" alt="">
+                        <div class="u-fun__label">{{ item }}</div>
+                    </div>
+                </div>
+                <div class="m-tips">
+                    <div class="u-tips__title">这一次，普通用户也能体验</div>
+                    <div class="u-tips__sub">[ 每项功能可试用X次 ]</div>
+                </div>
+            </div>
+            <div class="u-face" :class="`u-face-${item}`" v-for="(item,index) in 2" :key="item">
+                <img class="u-img" :src="require(`../../assets/img/celebration/4/Ellipse${index}.png`)" alt="">
+                <img class="u-code" src="../../assets/img/celebration/miniprogram.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="m-page-5 m-page-layout">
+            <img class="u-mark" src="../../assets/img/celebration/5/5.svg" alt="">
+            <img class="u-bg" src="../../assets/img/celebration/5/bg.jpg" alt="">
+            <div class="m-main">
+                <img class="u-title" src="../../assets/img/celebration/5/title.svg" alt="">
+                <div class="m-info">
+                    <img class="u-top" src="../../assets/img/celebration/5/page-title.png" alt="">
+                    <div class="u-top__tip">[ 非活动时期 ]</div>
+                    <img class="u-points" src="../../assets/img/celebration/5/points.png" alt="">
+                    <div class="u-points__tip">错过再等一整年！</div>
+                    <img class="u-box__logo" src="../../assets/img/celebration/5/box-logo.png" alt="">
+                </div>
+            </div>
+            <div class="u-face" :class="`u-face-${item}`" v-for="(item,index) in 2" :key="item">
+                <img class="u-img" :src="require(`../../assets/img/celebration/5/Ellipse${index}.png`)" alt="">
+                <img class="u-code" src="../../assets/img/celebration/miniprogram.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="m-page-6 m-page-layout">
+            <img class="u-mark" src="../../assets/img/celebration/6/6.svg" alt="">
+            <img class="u-bg" src="../../assets/img/celebration/6/bg.jpg" alt="">
+            <div class="m-main">
+                <img class="u-title" src="../../assets/img/celebration/6/title.svg" alt="">
+                <div class="m-info">
+                    <div class="u-top">B5 40P横线本 4选1</div>
+                    <div class="m-book">
+                        <img class="u-book" v-for="item in 4" :key="item"
+                             :src="require(`../../assets/img/celebration/6/book_${item}.png`)" alt="">
+                    </div>
+                    <div class="u-book__tip">活动期间开年费会员，每开通一次，均可领取一套精美笔记本（4选1）</div>
+                    <img class="u-open" src="../../assets/img/celebration/6/open.svg" alt="">
+                    <div class="u-time">[ 活动时间：2024.12.28~2025.2.28 ]</div>
+                    <img class="u-get" src="../../assets/img/celebration/get.png" alt="">
+                </div>
+            </div>
+            <div class="u-face" :class="`u-face-${item}`" v-for="(item,index) in 5" :key="item">
+                <img class="u-img" :src="require(`../../assets/img/celebration/6/Ellipse${index}.png`)" alt="">
+                <img class="u-code" src="../../assets/img/celebration/miniprogram.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="m-page-7 m-page-layout">
+            <img class="u-mark" src="../../assets/img/celebration/7/7.svg" alt="">
+            <img class="u-bg" src="../../assets/img/celebration/7/bg.png" alt="">
+            <div class="m-main">
+                <img class="u-title" src="../../assets/img/celebration/7/title.svg" alt="">
+                <div class="m-info">
+                    <div class="m-item">
+                        <img class="u-get__title" src="../../assets/img/celebration/7/get_1.png" alt="">
+                        <div class="m-text">
+                            <div class="u-text__title">使用200积分兑换1个月会员</div>
+                            <div class="u-text__sub">我的积分：1216</div>
+                            <div class="u-text__tip">（剩余领取次数：5）</div>
+                        </div>
+                        <img class="u-get" src="../../assets/img/celebration/get.png" alt="">
+                    </div>
+                    <div class="m-item">
+                        <img class="u-get__title" src="../../assets/img/celebration/7/get_2.png" alt="">
+                        <div class="m-text">
+                            <div class="u-text__title">于2021年12月28日之前注册的用户<br>可以领取3个月会员</div>
+                            <div class="u-text__sub">我的注册时间：2021年12月27日</div>
+                            <div class="u-text__tip">（剩余领取次数：1）</div>
+                        </div>
+                        <img class="u-get" src="../../assets/img/celebration/get.png" alt="">
+                    </div>
+                    <div class="m-item">
+                        <img class="u-get__title" src="../../assets/img/celebration/7/get_3.png" alt="">
+                        <div class="m-text">
+                            <div class="u-text__title">
+                                Lv4可以领1个月
+                                <br>
+                                Lv5可以领2个月
+                                <br>
+                                Lv6及以上可以领3个月
+                            </div>
+                            <div class="u-text__sub">我的等级：5</div>
+                            <div class="u-text__tip">（剩余领取次数：1）</div>
+                        </div>
+                        <img class="u-get" src="../../assets/img/celebration/get.png" alt="">
+                    </div>
+                </div>
+                <div class="u-time">[ 活动时间：2024.12.28~2025.2.28 ]</div>
+            </div>
+        </div>
+
+        <div class="m-page-8 m-page-layout">
+            <div class="m-author">
+                <div class="m-author__list" v-for="item in 12" :key="item">
+                    <img class="u-author" src="" v-for="item in 16" :key="item" alt="">
+                </div>
+                <img class="u-center__title" src="../../assets/img/celebration/8/center-title.svg" alt="">
+            </div>
+            <img class="u-bottom" src="../../assets/img/celebration/8/bottom.jpg" alt="">
         </div>
     </div>
 </template>
@@ -179,7 +345,8 @@ export default {
     components: {},
     data() {
         return {
-            items: ["《我恨剑网3》", "纪念勋章", "会员限定装扮", "魔盒会员新功能", "充会员返礼", "年费会员福利"],
+            items: ["《我恨剑网3", "纪念勋章", "会员限定装扮", "魔盒会员新功能", "充会员返礼", "年费会员福利"],
+            page4Arr: ["新功能提前体验", "团队应用高级功能", "个人战斗数据仓库", "魔盒助手高级功能", "专属虚拟装扮", "亲友名单无上限", "专属昵称尾巴标识", "签到双倍积分"],
         };
     },
     watch: {},
@@ -195,8 +362,9 @@ export default {
             const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
             console.log(scrollTop);
         },
-        playAudio() {
-            const audio = new Audio("path/to/your/audio/file.mp3");
+        playAudio(itemData) {
+            console.log(itemData);
+            const audio = new Audio(require(`../../assets/audio/celebration/C4_${itemData}.mp3`));
             audio.play();
         },
     },
